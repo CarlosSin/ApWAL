5d9b80827ed467740a070c53fc746e5820deaffe
Carlos <cvelascoa1600@alumno.ipn.mx>
2025-05-06 23:49:37 -0600

    cambios en el diseño de la interfaz del investigador

---
+++ src/app/inv-pages/components/menu-register/menu-register.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'menu-register',
+  imports: [],
+  templateUrl: './menu-register.component.html',
+})
+export class MenuRegisterComponent { }
--- src/app/inv-pages/pages/protocol-register/protocol-register.component.ts
+++ src/app/inv-pages/pages/protocol-register/protocol-register.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MenuRegisterComponent } from "../../components/menu-register/menu-register.component";
 
 @Component({
   selector: 'app-protocol-register',
-  imports: [],
+  imports: [MenuRegisterComponent],
   templateUrl: './protocol-register.component.html',
 })
 export class ProtocolRegisterComponent { }
