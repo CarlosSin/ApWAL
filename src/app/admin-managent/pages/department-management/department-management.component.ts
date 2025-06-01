7465515b2dfda8ff3f286e3ef3d0af6254bc56cc
carlos luna <clunag1800@alumno.ipn.mx>
2025-06-01 15:27:45 -0600

    se agrego cambios al registro de departamento y se agrego gestionm de depatamentos

--- src/app/admin-managent/pages/department-management/department-management.component.ts
+++ src/app/admin-managent/pages/department-management/department-management.component.ts
@@ -1,8 +1,47 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+interface departamentorules{
+  id_departamento:number,
+  nombre_departamento:string,
+  disponibilidad_departamento:boolean,
+}
 
 @Component({
   selector: 'app-department-management',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './department-management.component.html',
 })
-export class DepartmentManagementComponent { }
+export class DepartmentManagementComponent { 
+  mostrarFormulario = false;
+
+  nuevoDepartamento:departamentorules={
+    id_departamento: 0,
+    nombre_departamento: '',
+    disponibilidad_departamento: false
+  }
+
+  listaDepartamentos:departamentorules [] = [];
+  
+  abrirFormulario() {
+    console.log('¡Botón presionado!');
+    this.mostrarFormulario = true;
+  }
+  
+  cerrarFormulario() {
+    this.mostrarFormulario = false;
+  }
+  
+  agregarUsuario() {
+    if (this.nuevoDepartamento.disponibilidad_departamento && this.nuevoDepartamento.nombre_departamento) {
+      this.listaDepartamentos.push({ ...this.nuevoDepartamento });
+      this.nuevoDepartamento = { 
+        id_departamento: 0,
+        nombre_departamento: '',
+        disponibilidad_departamento: false
+      };
+      this.cerrarFormulario();
+    }
+  }
+}
