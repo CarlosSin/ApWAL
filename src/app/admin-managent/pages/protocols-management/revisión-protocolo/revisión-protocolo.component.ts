6965f567e618d056ed0822595bf23bb9081cd521
carlos luna <clunag1800@alumno.ipn.mx>
2025-06-05 01:14:05 -0600

    se agrego los botones faltantes asi como formatos faltantes y se valido el primer formulario que es el de usuarios pero hace falta ponerle los mensajes de advertencia

--- src/app/admin-managent/pages/protocols-management/revisión-protocolo/revisión-protocolo.component.ts
+++ src/app/admin-managent/pages/protocols-management/revisión-protocolo/revisión-protocolo.component.ts
@@ -1,8 +1,63 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+interface protocolrule{
+  ID_protocolo:string,
+  nombre_protocolo:string,
+  edo_protocolo:string,
+  fecha:string,
+}
+
 
 @Component({
   selector: 'app-revisión-protocolo',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './revisión-protocolo.component.html',
 })
-export class RevisiónProtocoloComponent { }
+export class RevisiónProtocoloComponent { 
+  
+  lista_protocolo_revicion:protocolrule [] = [];//lista de solicitudes en revicion
+  lista_resto_protocolos:protocolrule [] = [];// lsita de solicitudes aprovadas
+
+  lista_protocolos: protocolrule[]=[
+    {ID_protocolo:'1APPDe', nombre_protocolo:'Animalitos del bosque', edo_protocolo: null,fecha:'05-06-2025'},
+    {ID_protocolo:'1JKLP', nombre_protocolo:'Animalitos del bosque2', edo_protocolo: 'A',fecha:'05-06-2025'},
+    {ID_protocolo:'1JKLP', nombre_protocolo:'Animalitos del bosque3', edo_protocolo: 'A/R',fecha:'05-06-2025'},
+    {ID_protocolo:'1JKLP', nombre_protocolo:'Animalitos del bosque4', edo_protocolo: 'NA',fecha:'05-06-2025'}
+  ];
+  
+  constructor(){
+    this.clasificarprotocolos();
+  }
+
+  clasificarprotocolos(){
+    for (let protocolo of this.lista_protocolos) {
+      if(protocolo.edo_protocolo===null){
+        this.lista_protocolo_revicion.push({ ...protocolo});
+      }else{
+        this.lista_resto_protocolos.push({ ...protocolo});
+      }
+    }
+  }
+}
+
+/**
+
+export class AdministracionSolicitudesComponent {
+  
+  clasificarsolicitudes(){
+    //cuando la solicitud esta en revicion
+    for (let solicitud of this.nuevaSolicitud) {
+      if(solicitud.edo_solicitud===null){
+        this.lista_solicitud_revicion.push({ ...solicitud});
+      }else if (solicitud.edo_solicitud===true){
+        this.lista_solicitud_aprovada.push({ ...solicitud });
+      } else if(solicitud.edo_solicitud===false){
+        this.lista_solicitud_rechazada.push({ ...solicitud });
+      }
+    }    
+  }
+
+}
+ */
--- src/app/admin-managent/pages/users-management/users-management.component.ts
+++ src/app/admin-managent/pages/users-management/users-management.component.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule, FormsModule, Validators } from '@angular/forms';
 
-interface usuariorRules {
+interface usuarioRules {
   no_control: number,
   nombres:string,
   primer_apellido:string,
@@ -22,34 +22,37 @@ interface usuariorRules {
 
 @Component({
   selector: 'app-users-management',
-  imports: [ CommonModule, FormsModule],
+  imports: [ CommonModule, FormsModule,ReactiveFormsModule],
   templateUrl: './users-management.component.html',
 })
 
 export class UsersManagementComponent { 
  
   mostrarFormulario = false;
-
-  nuevoUsuario:usuariorRules = {
-    no_control: 0,
-    nombres: '',
-    primer_apellido:'',
-    segundo_apellido:'',
-    telefono: 0,
-    extension: 0,
-    fecha_registro: '',
-    gradoestudios: '',
-    correo: '',
-    nombre_usuario: '',
-    password: '',
-    estado_usuario: false,
-    departamento_usuario: 0,
-    rol: 0,
-    puede_iniciarsecion: false
-  };
-
   
-  listaUsuarios:usuariorRules [] = [];
+  //constructor(private fb: FormBuilder){}
+  private fb =inject(FormBuilder);
+
+  myForm = this.fb.group({
+    no_control: [0,[Validators.required]],
+    nombres: ['',[Validators.required]],
+    primer_apellido:['',[Validators.required]],
+    segundo_apellido:['',[Validators.required]],
+    telefono: [0,[Validators.required]],
+    extension: [0,[Validators.required]],
+    fecha_registro: ['',[Validators.required]],
+    gradoestudios: ['',[Validators.required]],
+    correo: ['',[Validators.required]],
+    nombre_usuario: ['',[Validators.required]],
+    password: ['',[Validators.required]],
+    estado_usuario: [false,[Validators.required]],
+    departamento_usuario: [0,[Validators.required]],
+    rol: [0,[Validators.required]],
+    puede_iniciarsecion: [false,[Validators.required]]
+    
+  })
+  
+  listaUsuarios: usuarioRules [] = [];
 
   abrirFormulario() {
     console.log('¡Botón presionado!');
@@ -61,25 +64,9 @@ export class UsersManagementComponent {
   }
 
   agregarUsuario() {
-    if (this.nuevoUsuario.nombres && this.nuevoUsuario.correo) {
-      this.listaUsuarios.push({ ...this.nuevoUsuario });
-      this.nuevoUsuario = { 
-        no_control: 0,
-        nombres: '',
-        primer_apellido:'',
-        segundo_apellido:'',
-        telefono: 0,
-        extension: 0,
-        fecha_registro: '',
-        gradoestudios: '',
-        correo: '',
-        nombre_usuario: '',
-        password: '',
-        estado_usuario: false,
-        departamento_usuario: 0,
-        rol: 0,
-        puede_iniciarsecion: false
-      };
+    if (this.myForm.valid) {
+      this.listaUsuarios.push({ ...this.myForm.value as usuarioRules});
+      this.myForm.reset();
       this.cerrarFormulario();
     }
   }
