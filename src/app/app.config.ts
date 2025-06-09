793f533d0c2a3d5df960b8a9c378518718cdf22d
David Tapia <david_tm02@hotmail.com>
2025-06-09 17:08:21 -0600

    Actualizacion

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,21 +1,13 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-<<<<<<< HEAD
 import { provideHttpClient } from '@angular/common/http';
-=======
-import { provideHttpClient } from '@angular/common/http'; // ✅ Agregar esto
->>>>>>> b2b5c31a4fce9c67134681a5b3d4c8f99009d6ae
-
-
 import { routes } from './app.routes';
 
 // export const appConfig: ApplicationConfig = {
 //   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
 // };
 export const appConfig: ApplicationConfig = {
-<<<<<<< HEAD
+
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),provideHttpClient()]
-=======
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient()]
->>>>>>> b2b5c31a4fce9c67134681a5b3d4c8f99009d6ae
+
 };
